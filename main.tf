1864705deb661f51c6c32291bec3f3bfcc29fe88
Siva123569 <nallasiva552@gmail.com>
2025-11-14 10:24:19 +0530

    Configure Azure backend for Terraform

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-   
-  }
+    resource_group_name  = "rg01"
+    storage_account_name = "siva1234567"
+    container_name       = "state"
+    key                  = "tf"
+  }
 }
